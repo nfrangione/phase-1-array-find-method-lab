143a2d5febd1eacf214beb987515e7420cdb3e74
Nicholas V Frangione <nfrangione8@gmail.com>
2021-02-23 16:23:30 -0800

    commit tree 5afa63e4194b5f95e2b85d6c8f9ac70180206ada

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+superbowlWin = (record) => {
+    let win = record.find(game => game.result === "W")
+    if (win === undefined){
+        return undefined;
+    }
+    else {
+        return win.year;
+    } 
+}
